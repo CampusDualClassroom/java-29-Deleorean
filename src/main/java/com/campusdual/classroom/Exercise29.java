f8f6dde6b70346b214eba952bf9d37e63972f512
Edugonzalez <eduu.gonzalez@gmail.com>
2025-02-03 10:28:16 +0100

    Completado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,17 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
+       int dividendo = 3;
+       int divisor = 0;
+        try {
+            int resultado = dividendo/divisor;
+
+        } catch (ArithmeticException e) {
+            System.out.println("No se puede dividir entre 0");
+        }
+        finally{
+            System.out.println("Programa acabado");
+        }
 
     }
 }
